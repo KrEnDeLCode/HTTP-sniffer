3accdbda1486bd75752570e18f6fa882771880b1
KrEnDeLCode <lobarev.vp@gmail.com>
2023-04-05 12:31:49 +0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string.h>
+#include <pcap/pcap.h>
+
+void my_callback(u_char *args, const struct pcap_pkthdr * pkthdr, const u_char * packet) 
+{ 
+	static int count = 1; 
+	std::cout <<  count++ << ",   ";
+	fflush(stdout);
+}
+
+int main(int argc, char *argv[]) {
+
+    char *device;
+    pcap_t *descr;
+    pcap_if_t *alldevs;
+    struct bpf_program fp;
+    char ip[13];
+    char mask[13];
+    bpf_u_int32 ip_raw;
+    bpf_u_int32 mask_raw;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    std::string filterstr = "port 80 or 443";
+    char *filter_exp;
+    struct in_addr address;
+    static int pckcnt = 1;
+
+
+    pcap_findalldevs(&alldevs, errbuf);
+    /* if (pcap_findalldevs(&alldevs, errbuf) != -1)
+    {
+        auto device = alldevs;
+        int i = 0;
+        for(pcap_if_t *d = alldevs; d != NULL; d = d->next)
+        {
+            std::cout << d->name << "\t" << d->addresses << "\t" << d->flags << std::endl;
+        }
+    } */
+    std::string dev = alldevs->name;
+    pcap_freealldevs(alldevs);
+    std::cout << "Device: " << dev << "\t";
+    device = &*dev.begin();
+
+    pcap_lookupnet(device, &ip_raw, &mask_raw, errbuf);
+    descr = pcap_open_live(device, BUFSIZ, 1, -1, errbuf);
+    if (descr == NULL){
+        std::cout << "pcap_open_life()\n" << errbuf << std::endl;
+        return 1;
+    }
+
+    address.s_addr = ip_raw;
+    strcpy(ip, inet_ntoa(address));
+    if (ip == NULL) {
+        perror("inet_ntoa");
+        return 1;
+    }
+    std::cout << "IP: " << ip << "\t";
+
+    address.s_addr = mask_raw;
+    strcpy(mask, inet_ntoa(address));
+    if (mask == NULL) {
+        perror("inet_ntoa");
+        return 1;
+    }
+    std::cout << "Netmask: " << mask << "\t";
+
+    filter_exp = &*filterstr.begin();
+    if(pcap_compile(descr, &fp, filter_exp, 0, mask_raw) == -1) {
+		std::cout << "\nError calling pcap_compile\n";
+		return 1;
+	}
+
+    if(pcap_setfilter(descr, &fp) == -1) {
+		std::cout << "\nError setting filter\n";
+		return 1;
+	}
+    std::cout << "Filter: " << filter_exp << "\n";
+    pcap_loop(descr, -1, my_callback, NULL);
+    std::cout << "\n";
+}
---
+++ out.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
