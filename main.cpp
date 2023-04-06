c69f1114883067403faa978e0eb6f83e4fc83b23
KrEnDeLCode <lobarev.vp@gmail.com>
2023-04-06 12:37:55 +0300

    Updated console output, new ip getting

--- main.cpp
+++ main.cpp
@@ -1,18 +1,112 @@
 #include <iostream>
 #include <cstdlib>
 #include <string.h>
+#include <vector>
 #include <pcap/pcap.h>
-#include "PtcHeaders.cpp"
+
+#define SIZE_ETHERNET 14
+
+const struct sniff_ethernet *ethernet; /* The ethernet header */
+const struct sniff_ip *ip; /* The IP header */
+const struct sniff_tcp *tcp; /* The TCP header */
+const char *payload; /* Packet payload */
+
+u_int size_ip;
+u_int size_tcp;
+
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+
+/* Ethernet header */
+struct sniff_ethernet {
+	u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+	u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+	u_short ether_type; /* IP? ARP? RARP? etc */
+};
+
+/* IP header */
+struct sniff_ip {
+	u_char ip_vhl;		/* version << 4 | header length >> 2 */
+	u_char ip_tos;		/* type of service */
+	u_short ip_len;		/* total length */
+	u_short ip_id;		/* identification */
+	u_short ip_off;		/* fragment offset field */
+#define IP_RF 0x8000		/* reserved fragment flag */
+#define IP_DF 0x4000		/* don't fragment flag */
+#define IP_MF 0x2000		/* more fragments flag */
+#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+	u_char ip_ttl;		/* time to live */
+	u_char ip_p;		/* protocol */
+	u_short ip_sum;		/* checksum */
+	struct in_addr ip_src,ip_dst; /* source and dest address */
+};
+#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
+
+/* TCP header */
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+	u_short th_sport;	/* source port */
+	u_short th_dport;	/* destination port */
+	tcp_seq th_seq;		/* sequence number */
+	tcp_seq th_ack;		/* acknowledgement number */
+	u_char th_offx2;	/* data offset, rsvd */
+#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+	u_char th_flags;
+#define TH_FIN 0x01
+#define TH_SYN 0x02
+#define TH_RST 0x04
+#define TH_PUSH 0x08
+#define TH_ACK 0x10
+#define TH_URG 0x20
+#define TH_ECE 0x40
+#define TH_CWR 0x80
+#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+	u_short th_win;		/* window */
+	u_short th_sum;		/* checksum */
+	u_short th_urp;		/* urgent pointer */
+};
+
+class Output{
+private:
+    in_addr ip;
+    int packet_in, packet_out;
+    int traffic_in, traffic_out;
+
+public:
+    Output(in_addr address, const struct pcap_pkthdr * pkthdr){
+        ip = address;
+        packet_in, packet_out = 0;
+        traffic_in, traffic_out = 0;
+    }
+
+};
 
 void my_callback(u_char *args, const struct pcap_pkthdr * pkthdr, const u_char * packet) 
 { 
 	static int count = 1;
     //system("clear");
-	std::cout << "Packet №: " <<  count++ << std::endl;
-    std::cout << "Packet length: " << pkthdr->len << std::endl;
-	std::cout << "Data length " <<  pkthdr->caplen << std::endl;
-    std::cout << std::endl;
-	fflush(stdout);
+	
+   // static Output console;
+
+    ethernet = (struct sniff_ethernet*)(packet);
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    /* if (size_ip < 20 || size_tcp < 20) {
+	    std::cout << "   * Invalid IP/TCP header length: "<< size_ip << " and " << size_tcp << "bytes\n";
+	    return;
+    } */
+  
+    std::cout << "Packet №: " <<  count << std::endl;
+    std::cout << "IP/TCP header length: "<< size_ip << " and " << size_tcp << " bytes\n";
+    std::cout << "Source IP-addres: " << pkthdr->len << std::endl;
+	std::cout << "Destenation IP-addres: " << pkthdr->caplen << std::endl << std::flush;
+    std::cout << "\33[A\33[2K\33[A\33[2K\33[A\33[2K\33[A\33[2K";
+
+    count++;
 }
 
 int main(int argc, char *argv[]) {
@@ -21,7 +115,7 @@ int main(int argc, char *argv[]) {
     pcap_t *descr;
     pcap_if_t *alldevs;
     struct bpf_program fp;
-    char ip[13];
+    char *ip;
     char mask[13];
     bpf_u_int32 ip_raw;
     bpf_u_int32 mask_raw;
@@ -35,19 +129,16 @@ int main(int argc, char *argv[]) {
 
 
     pcap_findalldevs(&alldevs, errbuf);
-    /* if (pcap_findalldevs(&alldevs, errbuf) != -1)
-    {
-        auto device = alldevs;
-        int i = 0;
-        for(pcap_if_t *d = alldevs; d != NULL; d = d->next)
-        {
-            std::cout << d->name << "\t" << d->addresses << "\t" << d->flags << std::endl;
-        }
-    } */
+    for(pcap_addr_t *a=alldevs->addresses; a!=NULL; a=a->next) {
+        if(a->addr->sa_family == AF_INET)
+            ip = inet_ntoa(((struct sockaddr_in*)a->addr)->sin_addr);
+    } 
     std::string dev = alldevs->name;
+    
     pcap_freealldevs(alldevs);
     std::cout << "Device: " << dev << "\t";
     device = &*dev.begin();
+    
 
     pcap_lookupnet(device, &ip_raw, &mask_raw, errbuf);
     descr = pcap_open_live(device, BUFSIZ, 1, -1, errbuf);
@@ -56,12 +147,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    address.s_addr = ip_raw;
-    strcpy(ip, inet_ntoa(address));
-    if (ip == NULL) {
-        perror("inet_ntoa");
-        return 1;
-    }
     std::cout << "IP: " << ip << "\t";
 
     address.s_addr = mask_raw;
@@ -83,15 +168,7 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
     std::cout << "Filter: " << filter_exp << "\n";
-    /* for(int i = 0; i < 1000; i++){
-        packet = pcap_next(descr, &header);
-        if (packet == NULL){ 
-            std::cout << "No packet found.\n";
-            continue;
-        }
-        std::cout << "Packet length: " << header.len << std::endl;
-        std::cout << "Captured packet length: " << header.caplen << std::endl;
-    } */
+    
     pcap_loop(descr, -1, my_callback, NULL);
     pcap_close(descr);
     std::cout << "\n";
